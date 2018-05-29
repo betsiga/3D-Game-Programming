a24bd4ef6b8180573f851b98544dbaf40dcd7ed2
betsiga <xiongsj@mail2.sysu.edu.cn>
2018-05-29 16:33:52 +0800

    homework7

---
+++ homework7/README.md
@@ -0,0 +1,12 @@
+# 简单粒子制作 #
+
+### 演示视频 ###
+
+[演示视频](http://v.youku.com/v_show/id_XMzYzMzEzNzM4MA==.html?spm=a2h3j.8428770.3416059.1 "")
+
+### 控制代码说明 ###
+
+代码挂载在子粒子系统star上，参数starRadius用于控制组件ParticleSystem中Shape模块的Radius属性。通过改变该值，光晕射出的“星光”的范围也随之改变。
+
+实现了简单的通过代码控制使粒子系统在不同场景下效果不一样。
+
---
+++ homework7/游戏源代码/粒子系统/Assets/Scripts/PSTextScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSTextScript : MonoBehaviour {
+
+    public float starRadius;
+    ParticleSystem star;
+    
+    // Use this for initialization
+    void Start () {
+        starRadius = 0.73f;
+        star = GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (star.shape.radius != starRadius)
+        {
+            star.Stop();
+            var shape = star.shape;
+            shape.radius = starRadius;
+            star.Play();
+        } 
+	}
+}
