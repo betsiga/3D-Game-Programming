4d8815bbf034f579ada77b48e9f6e4cdc85f364d
betsiga <xiongsj@mail2.sysu.edu.cn>
2018-06-05 18:12:20 +0800

    homework8

---
+++ homework8/游戏源代码/简单血条制作/Assets/IMGUIbloodbar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMGUIbloodbar : MonoBehaviour
+{
+
+    public float initblood = 0.0f;
+    private float currentblood;
+    private Rect BloodBar;
+    private Rect Add;
+    private Rect Minus;
+
+    void Start()
+    {
+        BloodBar = new Rect(360, 140, 50, 2);
+        Add = new Rect(330, 110, 50, 20);
+        Minus = new Rect(390, 110, 50, 20);
+        currentblood = initblood;
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(Add, "Add"))
+        {
+            if(currentblood + 0.1f > 1.0f)
+            {
+                currentblood = 1.0f;
+            }
+            else
+            {
+                currentblood += 0.1f;
+            }
+        }
+        if (GUI.Button(Minus, "Minus"))
+        {
+            if (currentblood - 0.1f < 0.0f)
+            {
+                currentblood = 0.0f;
+            }
+            else
+            {
+                currentblood -= 0.1f;
+            }
+        }
+
+        initblood = Mathf.Lerp(initblood, currentblood, 0.05f);
+        GUI.HorizontalScrollbar(BloodBar, 0.0f, initblood, 0.0f, 1.0f);
+    }
+}
---
+++ homework8/游戏源代码/简单血条制作/Assets/UGUIbloodbar.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UGUIbloodbar : MonoBehaviour
+{
+    public Button Add;
+    public Button Minus;
+
+    private void Start()
+    {
+        Add.onClick.AddListener(delegate () {
+            float blood = GameObject.FindGameObjectWithTag("bloodbar").GetComponent<Slider>().value;
+            blood -= 10;
+            if (blood < 0)
+            {
+                GameObject.FindGameObjectWithTag("bloodbar").GetComponent<Slider>().value = 0;
+            }
+            else
+            {
+                GameObject.FindGameObjectWithTag("bloodbar").GetComponent<Slider>().value = blood;
+            }
+
+        });
+        Minus.onClick.AddListener(delegate () {
+            float blood = GameObject.FindGameObjectWithTag("bloodbar").GetComponent<Slider>().value;
+            blood += 10;
+            if (blood > 100)
+            {
+                GameObject.FindGameObjectWithTag("bloodbar").GetComponent<Slider>().value = 100;
+            }
+            else
+            {
+                GameObject.FindGameObjectWithTag("bloodbar").GetComponent<Slider>().value = blood;
+            }
+
+        });
+    }
+
+    void Update()
+    {
+        this.transform.LookAt(Camera.main.transform.position);
+        
+    }
+}
